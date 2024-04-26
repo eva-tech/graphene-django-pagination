fefd4c8b5c00d399489639b67c8459ab1e2bc7dc
IvanDiazNoriega <angel.diaz@live.com.mx>
2024-04-25 19:29:52 -0600

    feat: changes count to len

--- graphene_django_pagination/connection_field.py
+++ graphene_django_pagination/connection_field.py
@@ -61,10 +61,7 @@ class DjangoPaginationConnectionField(DjangoFilterConnectionField):
     def resolve_connection(cls, connection, args, iterable, max_limit=None):
         iterable = maybe_queryset(iterable)
 
-        if isinstance(iterable, QuerySet):
-            _len = iterable.count()
-        else:
-            _len = len(iterable)
+        _len = len(iterable)
 
         ordering = args.get("ordering")
 
